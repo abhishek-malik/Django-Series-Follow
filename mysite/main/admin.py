923c721a5980645f708118acf6169cab6588a0ce
Abhishek Malik <maps.abhishek@gmail.com>
2019-04-08 23:59:49 +0530

    Admin editor

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from .models import Tutorial
 
+
 class TutorialAdmin(admin.ModelAdmin):
-    '''fields = ["tutorial_title",
-              "tutorial_published",
-              "tutorial_content"]
-    '''
+
     fieldsets = [
-        ("Title/date",{"fields": ["tutorial_title","tutorial_published"]}),
-        ("Content",{"fields": ["tutorial_content"]})
+        ("Title/date", {'fields': ["tutorial_title", "tutorial_published"]}),
+        ("Content", {"fields": ["tutorial_content"]})
     ]
-# Register your models here.
-admin.site.register(Tutorial, TutorialAdmin)
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+        }
+
+
+admin.site.register(Tutorial,TutorialAdmin)
